07c04e232fe231ec3d8842769934a3a4c3ca9fd8
Ernest Okosobo <okosobo.ernest@gmail.com>
2021-09-05 12:02:39 +0100

    Checkout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Sketchers';
-  constructor() {}
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
-   
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('initialized basket');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
   }
 }
